14177a034612475d5b9f46b04c150365727fa7b5
spandalai26 <spandalai26@gmail.com>
2023-03-03 20:13:35 +0000

    Implement Rollbar logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,12 @@ import watchtower
 import logging
 from time import strftime
 
+# For Rollbar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -74,6 +80,29 @@ cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
 LOGGER.addHandler(console_handler)
 LOGGER.addHandler(cw_handler)
 LOGGER.info("TEST LOG")
+
+#Configure Logging for Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
